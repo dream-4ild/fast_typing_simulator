f07bf564aae88280d5912b276cc436d23d3dccd5
dmitry <grishin.ds@phystech.edu>
2024-04-27 18:04:49 +0300

    add screeninfo

--- start.py
+++ start.py
@@ -1,6 +1,13 @@
 from src.game import game
+from screeninfo import get_monitors
+from src.constants import WIDTH, HEIGHT
 
 if __name__ == '__main__':
+    monitor = get_monitors()[0]
+
+    WIDTH = int(monitor.width * 0.625)
+    HEIGHT = int(monitor.height * 0.625)
+
     gm = game()
     print("Good buy!")
 
